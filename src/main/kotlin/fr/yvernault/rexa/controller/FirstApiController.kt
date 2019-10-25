cbfd91c59682e1017884cc58ec5eb0bb5bade46e
Bertrand yvernault <bertrand.yvernault@gmail.com>
2019-10-25 21:37:31 +0200

    endpoint to print all the information (scans and assessors) from a project id

--- src/main/kotlin/fr/yvernault/rexa/controller/FirstApiController.kt
+++ src/main/kotlin/fr/yvernault/rexa/controller/FirstApiController.kt
@@ -1,6 +1,7 @@
 package fr.yvernault.rexa.controller
 
-import fr.yvernault.daxapp.services.scan.impl.ScanServiceImpl
+import fr.yvernault.daxapp.services.scan.impl.AssessorService
+import fr.yvernault.daxapp.services.scan.impl.ScanService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
@@ -12,13 +13,48 @@ class FirstApiController {
     @GetMapping("/greeting")
     fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) = "Hello, $name"
 
-    @GetMapping("/project/{id}")
-    fun getProject(@PathVariable id: String){
-        val scansProject = ScanServiceImpl.get_project_scans(id)
-        if(scansProject==null){
-            print("The project : $id doesn't exist")
-        }else{
-            println("SCANS : $scansProject")
+    @GetMapping("/projects/{id}")
+    fun getXnatInfo(@PathVariable id: String){
+
+        /*
+        Get all the project informations
+
+        :param id: ID of a project on XNAT
+        :return: Json we differents information in the project
+         */
+
+        val scansProject = ScanService.get_project_scans(id)
+
+        // Number of scan
+        val nbScans = scansProject?.size
+
+        // Number of Session
+        val nbSession = scansProject?.groupBy { it.sessionLabel }?.size
+
+        // Number of Subject
+        val nbSubject = scansProject?.groupBy { it.subjectLabel }?.size
+
+        // Dico of scan by type
+        val dicoScanByType = scansProject?.groupBy { it.type }
+
+        println("Scans : $nbScans")
+        println("Sessions : $nbSession")
+        println("Subject : $nbSubject")
+        dicoScanByType?.forEach {
+            k, v ->
+            println("$k = ${v.size}")
         }
+
+        val assessorList = AssessorService.get_project_assessors(id)
+
+        val dicoAssessorByType = assessorList?.groupBy { it.type }
+
+        dicoAssessorByType?.forEach{
+            k, v ->
+            println("$k = ${v.size}")
+        }
+
+
     }
+
 }
--- src/main/kotlin/fr/yvernault/rexa/models/Scan.kt
+++ src/main/kotlin/fr/yvernault/rexa/models/Scan.kt
@@ -1,16 +1,24 @@
-package fr.yvernault.daxapp.models
+package fr.yvernault.rexa.models
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 data class Scan(
+        @JsonProperty("session_ID") var sessionId: String? = "",
         @JsonProperty("ID") var id: String? = "",
-        @JsonProperty("xsiType") var type: String = "",
-        @JsonProperty("insert_date") var insertDate: String = "",
+        @JsonProperty("subject_label") var subjectLabel: String? = "",
         @JsonProperty("URI") var uri: String? = "",
-        @JsonProperty("xnat:subjectassessordata/id") var subjectId: String? = "",
+        @JsonProperty("xnat:imagesessiondata/subject_id") var subjectId: String? = "",
+        @JsonProperty("xnat:imagescandata/type") var type: String? = "",
+        @JsonProperty("xnat:imagescandata/quality") var quality: String? = "",
+        @JsonProperty("xnat:imagescandata/note") var note: String? = "",
+        @JsonProperty("xnat:imagescandata/frames") var frames: String? = "",
+        @JsonProperty("xnat:imagescandata/series_description") var description: String? = "",
+        @JsonProperty("xnat:imagescandata/file/label") var file: String? = "",
+        @JsonProperty("label") var sessionLabel: String? = "",
         var date: String? = "",
         var project: String = "",
-        var label: String =""
-)
+        var xsiType: String=""
+) {
+}
