d7083dfbf7cc7164bbd29aca4c9eab4b9fbab88f
Bertrand yvernault <bertrand.yvernault@gmail.com>
2020-03-23 21:10:59 +0100

    Update for the migration file to create user table with a test data

---
+++ src/main/resources/db/migration/V1_2__data_user.sql
@@ -0,0 +1,3 @@
+-- V1_3__settings_data.sql
+INSERT INTO rexa.user VALUES ('beni@gmail.com', '$2a$04$zZDhsrIA9Ij/k7g.O.iqZuMTubxFhIW/EVoyPmQofw9x8sZ/MenLe');
+INSERT INTO rexa.user VALUES ('beber@gmail.com', '$2a$04$/FCeArg6BcWT7tYqChau5uhLlVaxOa4c6v5gO19mTAEW/4BPMXWiy');
