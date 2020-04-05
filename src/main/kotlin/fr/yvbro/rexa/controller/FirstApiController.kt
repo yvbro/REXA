fa0d4adfc0e04c7d610b9e6b315d1cce1751d626
Benjamin Yvernault <byvernault@gmail.com>
2020-04-05 19:04:30 +0200

    [REXA-24] Refacto

--- src/main/kotlin/fr/yvbro/rexa/controller/FirstApiController.kt
+++ src/main/kotlin/fr/yvbro/rexa/controller/FirstApiController.kt
@@ -1,6 +1,6 @@
 package fr.yvbro.rexa.controller
 
-import fr.yvbro.rexa.DTO.XnatInfoDTO
+import fr.yvbro.rexa.controller.output.XnatInfoDto
 import fr.yvbro.rexa.service.XnatService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.RestController
 class FirstApiController(private val xnatService: XnatService) {
 
     @GetMapping("/projects/{id}")
-    fun getXnatInfo(@PathVariable id: String): XnatInfoDTO {
+    fun getXnatInfo(@PathVariable id: String): XnatInfoDto {
 
         val scans = xnatService.getScansForProject(id)
         val assessors = xnatService.getAssessorsForProject(id)
 
-        return XnatInfoDTO(
+        return XnatInfoDto(
                 scans.size,
                 scans.groupBy { it.sessionLabel }.size,
                 scans.groupBy { it.subjectLabel }.size,
