2d88567fe0fc4693a6f1f9933fbe1f4fc88f0e38
Bertrand yvernault <bertrand.yvernault@gmail.com>
2020-07-05 16:07:04 +0200

    [REXA-22] add material ui for the settigns view

--- frontend/src/settings/dump/SettingsDetails.js
+++ frontend/src/settings/dump/SettingsDetails.js
@@ -1,20 +1,50 @@
 import React, { useEffect,useState } from "react"
 
-import { Container, Button, Form, Col, Row } from "react-bootstrap"
+import {Grid, Card, Button, TextField, makeStyles} from "@material-ui/core";
 import { useDispatch, useSelector } from "react-redux"
 
 import { fetchSettings, updateSettings } from "../redux/settingsDuck"
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+      minHeight: '100vh',
+  },
+  card: {
+      width: 400,
+      height: 400,
+      borderRadius: "16px",
+  },
+  form: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+  },
+  text: {
+      margin: theme.spacing(1),
+      width: 350,
+  },
+  button: {
+      width: 350,
+      margin: theme.spacing(1),
+  },
+  header: {
+    textAlign: "center",
+  }
+}));
+
 export const SettingsDetails = () => {
   const dispatch = useDispatch()
+  const classes = useStyles();
   const { xnatUsername, xnatHost, loading } = useSelector((state) => ({
     xnatUsername: state.settings.xnatUser,
     xnatHost: state.settings.xnatHost,
     loading: state.settings.loading,
   }))
-  const [disable, setDisable] = useState(false);
-  const [newUsername, setNewUsername] = useState("admin");
-  const [newHost, setNewHost] = useState("http://localhost");
+  const [username, setUsername] = useState(-1);
+  const [host, setHost] = useState(-1);
+  const [password, setPassword] = useState("");
+  const [errorPassword, setErrorPassword] = useState(false);
 
   useEffect(() => {
     dispatch(fetchSettings());
@@ -27,11 +57,72 @@ export const SettingsDetails = () => {
   function handleSubmit(event){
     event.preventDefault();
 
-    updateSettings(newUsername, newHost)
+  if(!password){
+    setErrorPassword(true);
+  }else{
+    updateSettings(username === -1 ? xnatUsername : username, host === -1 ? xnatHost : host)
+  }
   };
 
   return (
-    <Container>
+    <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justify="center"
+            className={classes.root}
+        >
+            <Grid item xs={3}>
+                <Card className={classes.card}>
+                    <div className={classes.header}>
+                        <h1>
+                            <span>Settings</span>
+                        </h1>
+                    </div>
+                    <form className={classes.form} noValidate autoComplete="off" onSubmit={handleSubmit}>
+                        <TextField id="username-id"
+                                   type="username"
+                                   name="username"
+                                   label="username"
+                                   variant="outlined"
+                                   defaultValue={xnatUsername}
+                                   onChange={e => setUsername(e.target.value)}
+                                   className={classes.text}/>
+                        <TextField
+                            id="host-id"
+                            name="host"
+                            label="host"
+                            type="text"
+                            variant="outlined"
+                            defaultValue={xnatHost}
+                            onChange={e => setHost(e.target.value)}
+                            className={classes.text}
+                        />                        
+                        <TextField
+                            id="filled-password-input"
+                            name="password"
+                            label="Password"
+                            type="password"
+                            variant="outlined"
+                            error={errorPassword}
+                            onChange={e => setPassword(e.target.value)}
+                            className={classes.text}
+                            helperText="password is required."
+                        />
+                        <Button
+                            type="submit"
+                            variant="outlined"
+                            color="primary"
+                            disabled={(host === -1 || host === xnatHost) && (username === -1 || username === xnatUsername)}
+                            className={classes.button}>
+                            Save
+                        </Button>
+                    </form>
+                </Card>
+            </Grid>
+        </Grid>
+/*     <Container>
       <form onSubmit={handleSubmit}>
         <Form>
           <Form.Group as={Row} controlId="formXnatUsername">
@@ -42,7 +133,7 @@ export const SettingsDetails = () => {
               <Form.Control
                 type="text"
                 defaultValue={xnatUsername}
-                onChange={e => setNewUsername(e.target.value)}
+                onChange={e => setUsername(e.target.value)}
                 />
             </Col>
           </Form.Group>
@@ -55,17 +146,17 @@ export const SettingsDetails = () => {
               <Form.Control
                 type="text"
                 defaultValue={xnatHost}
-                onChange={e => setNewHost(e.target.value)}
+                onChange={e => setHost(e.target.value)}
               />
             </Col>
           </Form.Group>
 
-          <Button disabled={disable} type="submit" size="lg">
+          <Button disabled={false} type="submit" size="lg">
             Save
           </Button>
         </Form>
       </form>
-    </Container>
+    </Container> */
   )
 }
 
