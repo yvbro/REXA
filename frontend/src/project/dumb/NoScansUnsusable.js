191ad818c68d97ab38f0b124764314a33f7e92bc
Benjamin Yvernault <byvernault@gmail.com>
2020-07-27 14:05:02 +0200

    [REXA-32] Add composant when no scan unusable

---
+++ frontend/src/project/dumb/NoScansUnsusable.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import { CardContent, Typography } from '@material-ui/core';
+import InfoIcon from '@material-ui/icons/Info';
+import grey from '@material-ui/core/colors/grey';
+
+const useStyles = makeStyles(() => ({
+    alignItemsAndJustifyContent: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        color: grey[500],
+        height: 350,
+    },
+    iconDef: {
+        fontSize: 80,
+    },
+}));
+
+export const NoScansUnusable = () => {
+    const classes = useStyles();
+
+    return (
+        <CardContent className={classes.alignItemsAndJustifyContent}>
+            <InfoIcon className={classes.iconDef} />
+            <Typography variant="h5" component="h2">
+                No scans found
+            </Typography>
+        </CardContent>
+    );
+};
--- frontend/src/project/dumb/ProjectScanUnusable.js
+++ frontend/src/project/dumb/ProjectScanUnusable.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import {
     Card,
 } from '@material-ui/core';
 import ScanUnusablePieChart from '../../chart/ScanUnusablePieChart';
-import { extractUnusableScanTypes } from '../../utils/xnat';
+import {extractUnusableScanTypes} from '../../utils/xnat';
+import {NoScansUnusable} from "./NoScansUnsusable";
 
 const useStyles = makeStyles({
     root: {
@@ -20,7 +21,7 @@ const useStyles = makeStyles({
     },
 });
 
-export const ProjectScanUnusable = ({ scans }) => {
+export const ProjectScanUnusable = ({scans}) => {
     const classes = useStyles();
 
     let unusableScans = [];
@@ -32,7 +33,12 @@ export const ProjectScanUnusable = ({ scans }) => {
         <>
             <h3>Scan types unusables</h3>
             <Card className={classes.cardInfo}>
-                <ScanUnusablePieChart unusableScans={unusableScans}/>
+                {unusableScans && unusableScans.length > 0 ? (
+                    <ScanUnusablePieChart unusableScans={unusableScans}/>
+                ) : (
+                    <NoScansUnusable/>
+                )
+                }
             </Card>
         </>
     );
