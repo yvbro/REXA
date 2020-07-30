4c124162bae3a8cd2eec5b07de6c1a252eabad39
Benjamin Yvernault <byvernault@gmail.com>
2020-07-30 07:19:05 +0200

    [REXA-32] Scroll for unknown status

--- frontend/src/project/dumb/ListUnknownProcessorStatus.js
+++ frontend/src/project/dumb/ListUnknownProcessorStatus.js
@@ -1,37 +1,48 @@
 import React from 'react';
 
-import {makeStyles, Card, ListItem, ListItemText, ListItemIcon} from '@material-ui/core';
+import {
+    makeStyles,
+    Card,
+    List,
+    ListItem,
+    ListItemText,
+    ListItemIcon,
+} from '@material-ui/core';
 import LabelOffIcon from '@material-ui/icons/LabelOff';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
 const useStyles = makeStyles({
     cardInfo: {
         borderRadius: '16px',
     },
+    listScrollable: {
+        maxHeight: '100%',
+        overflow: 'auto',
+    },
 });
 
-export const ListUnknownProcessorStatus = ({unknownStatus}) => {
+export const ListUnknownProcessorStatus = ({ unknownStatus }) => {
     const classes = useStyles();
 
     return (
         <>
             <h3>Unknown Processing status</h3>
             <Card className={classes.cardInfo}>
-                {unknownStatus && unknownStatus.map(status => (
-                <ListItem key={status}>
-                            <ListItemIcon>
-                                <LabelOffIcon />
-                            </ListItemIcon>
-                            <ListItemText
-                                primary={status}
-                            />
-                </ListItem>)
-                )}
+                <List className={classes.listScrollable}>
+                    {unknownStatus &&
+                        unknownStatus.map((status) => (
+                            <ListItem key={status}>
+                                <ListItemIcon>
+                                    <LabelOffIcon />
+                                </ListItemIcon>
+                                <ListItemText primary={status} />
+                            </ListItem>
+                        ))}
+                </List>
             </Card>
         </>
     );
 };
-
 
 ListUnknownProcessorStatus.propTypes = {
     unknownStatus: PropTypes.array.isRequired,
