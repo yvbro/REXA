0f0347f25ae8f09b739819ca0171cdaaba620ef6
Bertrand yvernault <bertrand.yvernault@gmail.com>
2020-11-13 20:48:04 +0100

    [REXA-46] add encryption and decrytage of the xnat password

--- src/main/kotlin/fr/yvbro/rexa/repository/mapper/UserSettingsMapper.kt
+++ src/main/kotlin/fr/yvbro/rexa/repository/mapper/UserSettingsMapper.kt
@@ -1,17 +1,19 @@
 package fr.yvbro.rexa.repository.mapper
 
+import fr.yvbro.rexa.config.Properties
 import fr.yvbro.rexa.jooq.generated.Tables
 import fr.yvbro.rexa.model.UserSettings
+import fr.yvbro.rexa.security.AESEncryptionDecryption
 import org.jooq.Record
 import org.jooq.RecordMapper
 import org.springframework.stereotype.Component
 
 @Component
-class UserSettingsMapper : RecordMapper<Record, UserSettings> {
+class UserSettingsMapper(private val securityConfiguration: AESEncryptionDecryption, private val properties: Properties) : RecordMapper<Record, UserSettings> {
     override fun map(record: Record?): UserSettings {
         return UserSettings.Builder()
                 .xnatUsername(record?.get(Tables.USER_SETTINGS.XNAT_USERNAME))
-                .xnatPassword(record?.get(Tables.USER_SETTINGS.XNAT_PASSWORD))
+                .xnatPassword(securityConfiguration.decrypt(record?.get(Tables.USER_SETTINGS.XNAT_PASSWORD), properties.secret))
                 .xnatHost(record?.get(Tables.USER_SETTINGS.XNAT_URL))
                 .userId(record?.get(Tables.USER_SETTINGS.USER_ID))
                 .build()
--- src/main/kotlin/fr/yvbro/rexa/service/UserSettingsService.kt
+++ src/main/kotlin/fr/yvbro/rexa/service/UserSettingsService.kt
@@ -1,19 +1,23 @@
 package fr.yvbro.rexa.service
 
+import fr.yvbro.rexa.config.Properties
 import fr.yvbro.rexa.model.UserSettings
 import fr.yvbro.rexa.repository.UserSettingsRepository
+import fr.yvbro.rexa.security.AESEncryptionDecryption
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class UserSettingsService(private val xnatRepository: UserSettingsRepository) {
+class UserSettingsService(private val xnatRepository: UserSettingsRepository,
+                          private val securityConfiguration: AESEncryptionDecryption,
+                          private val properties: Properties) {
 
     fun getXnatSettings(userId: UUID?): UserSettings {
         return xnatRepository.getSettingsByUserId(userId)
     }
 
     fun upsertXnatSettings(userId: UUID?, xnatUsername: String?, xnatHost: String?, xnatPassword: String?) {
-        xnatRepository.upsert(userId, xnatUsername, xnatHost, xnatPassword)
+        xnatRepository.upsert(userId, xnatUsername, xnatHost, securityConfiguration.encrypt(xnatPassword!!, properties.secret))
     }
 
 }
