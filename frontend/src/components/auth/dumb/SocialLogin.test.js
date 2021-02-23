70c35069d62e5149cf464c999531fab8d29880b8
Benjamin Yvernault <byvernault@gmail.com>
2021-02-23 19:22:03 +0100

    [REXA 30] Add test to frontend  (#48)
    I added some tests with the structure (test-utils.js) for few files:
    - SocialLogin (simple dumb component)
    - UsersManagementPage (page for one of the routes)
    - UserListPage (table to show the user)
    - userDuck (test a duck)
    
    Each of the tests starts with testing the snapshot of the component (new file in the directory for the snapshot).
    
    I probably forgot some tests cases on the UserListPage and I will add them if I can today (click on the add user button).
    
    I am using react testing library as well as jest to mock.

---
+++ frontend/src/components/auth/dumb/SocialLogin.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {renderWithStore, cleanup} from '../../../helpers/test/test-utils';
+import '@testing-library/jest-dom/extend-expect';
+
+import SocialLogin from './SocialLogin';
+
+import { GOOGLE_AUTH_URL } from '../../../helpers/constants/index';
+
+describe('The SocialLogin component', () => {
+    afterEach(cleanup);
+
+    it('should take a snapshot', () => {
+        const { asFragment } = renderWithStore(<SocialLogin />);
+
+        expect(asFragment(<SocialLogin />)).toMatchSnapshot();
+    });
+
+    it('should display a link as a button with the text "Sign in with Google"', () => {
+        const { getByRole } = renderWithStore(<SocialLogin />);
+
+        expect(getByRole('link')).toHaveTextContent('Sign in with Google');
+        expect(getByRole('link')).toHaveAttribute('href', GOOGLE_AUTH_URL);
+    });
+
+});
---
+++ frontend/src/components/management/smart/UserListPage.test.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { cleanup, fireEvent, renderWithStore, render, makeTestStore } from '../../../helpers/test/test-utils';
+import '@testing-library/jest-dom/extend-expect';
+
+import UserListPage from './UserListPage';
+import { SWITCH_ENABLED_USER } from '../redux/userDuck';
+import { pending } from '../../../helpers/promise';
+
+const TEST_USERS = [
+    { email: 'admin@test.com', roles: ['ADMIN', 'USER'], enabled: true},
+    { email: 'user@test.com', roles: ['USER'], enabled: true},
+    { email: 'disabled@test.com', roles: ['USER'], enabled: false},
+];
+
+const TEST_ACTION = {
+    type: pending(SWITCH_ENABLED_USER),
+    payload: { userEmail: TEST_USERS[2].email, enabled: !TEST_USERS[2].enabled }
+};
+
+const REGULAR_STATE = {
+    user: {
+        data: TEST_USERS,
+        loading: false
+    }
+};
+
+describe('The UserListPage component', () => {
+    
+    afterEach(cleanup);
+
+    it('should take a snapshot', () => {
+        const { asFragment } = renderWithStore(<UserListPage />, REGULAR_STATE);
+
+        expect(asFragment(<UserListPage />)).toMatchSnapshot();
+    });
+
+    it('should display user table properly', () => {
+        const { getByRole } = renderWithStore(<UserListPage />, REGULAR_STATE);
+
+        expect(getByRole('heading')).toHaveTextContent('User Management');
+        expect(getByRole('table')).toBeInTheDocument();
+        expect(getByRole('row', { name: 'admin@test.com ADMIN,USER primary checkbox'})).toBeInTheDocument();
+        expect(getByRole('row', { name: 'user@test.com USER primary checkbox'})).toBeInTheDocument();
+        expect(getByRole('row', { name: 'disabled@test.com USER primary checkbox'})).toBeInTheDocument();
+    });
+
+    it('should disabled checkbox if admin', () => {
+        const { getAllByRole } = renderWithStore(<UserListPage />, REGULAR_STATE);
+
+        expect(getAllByRole('checkbox')[0]).toBeDisabled();
+    });
+
+
+    it('should enabled checkbox if not admin', () => {
+        const { getAllByRole } = renderWithStore(<UserListPage />, REGULAR_STATE);
+
+        expect(getAllByRole('checkbox')[1]).not.toBeDisabled();
+        expect(getAllByRole('checkbox')[2]).not.toBeDisabled();
+    });
+
+    it('should checked checkbox if user enabled', () => {
+        const { getAllByRole } = renderWithStore(<UserListPage />, REGULAR_STATE);
+
+        expect(getAllByRole('checkbox')[0]).toHaveAttribute('checked');
+    });
+
+    it('should checked checkbox if user disabled', () => {
+        const { getAllByRole } = renderWithStore(<UserListPage />, REGULAR_STATE);
+
+        expect(getAllByRole('checkbox')[2]).not.toHaveAttribute('checked');
+    });
+
+    it('should dispatch action if click on checkbox', () => {
+        const store = makeTestStore(REGULAR_STATE);
+
+        const { getAllByRole } = render(<UserListPage />, { store });
+
+        const checkbox = getAllByRole('checkbox')[2];
+        expect(checkbox).not.toHaveAttribute('checked');
+
+        fireEvent.click(checkbox);
+
+        expect(store.getActions()).toStrictEqual([TEST_ACTION]);
+    });
+});
